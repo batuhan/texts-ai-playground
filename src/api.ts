784411b0595f906e6b4d75494f7be51fc433cf75
Alper Değre <alper.degre.dev@gmail.com>
2023-10-17 18:37:16 +0300

    Add title generation to threads using gpt-3.5 completions

--- src/api.ts
+++ src/api.ts
@@ -16,27 +16,28 @@ import {
   Thread,
   ThreadFolderName,
   UserID,
-} from '@textshq/platform-sdk'
-import { orderBy } from 'lodash'
-import OpenAI from 'openai'
-import { randomUUID as uuid } from 'crypto'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+  texts,
+} from "@textshq/platform-sdk";
+import { orderBy } from "lodash";
+import OpenAI from "openai";
+import { randomUUID as uuid } from "crypto";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 import {
   MODELS,
   SELF_ID,
   OPENAI_GPT_4_SVG_DATA_URI,
   OPENAI_SVG_DATA_URI,
-} from './constants'
+} from "./constants";
 
 function getModelImage(modelID: string) {
   switch (modelID) {
-    case 'gpt-3.5-turbo':
-    case 'gpt-3.5-turbo-16k':
-      return OPENAI_SVG_DATA_URI
-    case 'gpt-4':
-      return OPENAI_GPT_4_SVG_DATA_URI
+    case "gpt-3.5-turbo":
+    case "gpt-3.5-turbo-16k":
+      return OPENAI_SVG_DATA_URI;
+    case "gpt-4":
+      return OPENAI_GPT_4_SVG_DATA_URI;
     default:
-      return OPENAI_SVG_DATA_URI
+      return OPENAI_SVG_DATA_URI;
   }
 }
 
@@ -45,75 +46,78 @@ function getDefaultMessage(modelID: string): Message {
     id: uuid(),
     timestamp: new Date(),
     text: `This is the start of your conversation with ${
-      MODELS.find(mdl => mdl.id === modelID).fullName
+      MODELS.find((mdl) => mdl.id === modelID).fullName
     }. You can ask it anything you want!`,
-    senderID: 'ai',
+    senderID: "ai",
     isSender: false,
     threadID: modelID,
-  }
+  };
 }
 
 export default class ChatGPT implements PlatformAPI {
-  private currentUser: CurrentUser
+  private currentUser: CurrentUser;
 
-  private provider = 'openai'
+  private provider = "openai";
 
-  private apiKey: string
+  private apiKey: string;
 
-  private threads = new Map<Thread['id'], Thread>()
+  private threads = new Map<Thread["id"], Thread>();
 
-  private messages = new Map<Thread['id'], Message[]>()
+  private messages = new Map<Thread["id"], Message[]>();
 
-  private openai: OpenAI
+  private openai: OpenAI;
 
-  private eventHandler: OnServerEventCallback
+  private eventHandler: OnServerEventCallback;
 
   init = (session: SerializedSession) => {
     if (session) {
-      this.currentUser = session.user
-      this.provider = session.provider
+      this.currentUser = session.user;
+      this.provider = session.provider;
+      this.apiKey = session.apiKey;
       this.openai = new OpenAI({
         apiKey: session.apiKey,
-      })
+      });
     }
-  }
+  };
 
   login = (creds: LoginCreds): LoginResult => {
-    const loginCreds = 'custom' in creds
-      && creds.custom
-      && creds.custom.apiKey
-      && creds.custom.provider
-    if (!loginCreds) return { type: 'error', errorMessage: 'Invalid credentials' }
+    const loginCreds =
+      "custom" in creds &&
+      creds.custom &&
+      creds.custom.apiKey &&
+      creds.custom.provider;
+    if (!loginCreds)
+      return { type: "error", errorMessage: "Invalid credentials" };
 
-    this.provider = creds.custom.provider
-    this.apiKey = creds.custom.apiKey
-    const displayText = `${creds.custom.provider} ${creds.custom.label}`
+    this.provider = creds.custom.provider;
+    this.apiKey = creds.custom.apiKey;
+    const displayText = `${creds.custom.provider} ${creds.custom.label}`;
     this.currentUser = {
       id: `${this.provider}-${this.apiKey}`, // should uuid or smth
       displayText,
       username: creds.custom.username,
-    }
+    };
 
     this.openai = new OpenAI({
       apiKey: creds.custom.apiKey,
-    })
+    });
 
-    return { type: 'success' }
-  }
+    return { type: "success" };
+  };
 
-  dispose = () => {}
+  dispose = () => {};
 
-  getCurrentUser = () => this.currentUser
+  getCurrentUser = () => this.currentUser;
 
   serializeSession = () => ({
     user: this.currentUser,
     provider: this.provider,
     apiKey: this.apiKey,
-  })
+  });
 
   subscribeToEvents = (onEvent: OnServerEventCallback) => {
-    this.eventHandler = onEvent
-  }
+    this.eventHandler = onEvent;
+  };
 
   getThreads = (inboxName: ThreadFolderName) => {
     if (inboxName === InboxName.REQUESTS) {
@@ -121,30 +125,30 @@ export default class ChatGPT implements PlatformAPI {
         items: [],
         hasMore: false,
         oldestCursor: undefined,
-      }
+      };
     }
     return {
       items: [...this.threads.values()],
       hasMore: false,
       oldestCursor: undefined,
-    }
-  }
+    };
+  };
 
   getMessages = async (
     threadID: string,
-    pagination: PaginationArg,
+    pagination: PaginationArg
   ): Promise<Paginated<Message>> => ({
-    items: orderBy(this.messages.get(threadID) || [], 'timestamp'),
+    items: orderBy(this.messages.get(threadID) || [], "timestamp"),
     hasMore: false,
-  })
+  });
 
-  searchUsers = async () => MODELS
+  searchUsers = async () => MODELS;
 
   createThread = async (userIDs: UserID[], title: string, message: string) => {
-    const modelID = userIDs[0]
+    const modelID = userIDs[0];
     const thread: Thread = {
       id: uuid(),
-      type: 'single',
+      type: "single",
       timestamp: new Date(),
       description: `Chat with ${modelID}`,
       messages: {
@@ -156,7 +160,9 @@ export default class ChatGPT implements PlatformAPI {
         items: [
           {
             id: modelID,
-            fullName: `${MODELS.find(mdl => mdl.id === modelID).fullName} (${Date.now()})`,
+            fullName: `${
+              MODELS.find((mdl) => mdl.id === modelID).fullName
+            } (${Date.now()})`,
             imgURL: getModelImage(modelID),
           },
         ],
@@ -165,20 +171,21 @@ export default class ChatGPT implements PlatformAPI {
       isReadOnly: false,
       extra: {
         aiModelId: modelID,
+        titleGenerated: false,
       },
-    }
-    this.threads.set(thread.id, thread)
-    return thread
-  }
+    };
+    this.threads.set(thread.id, thread);
+    return thread;
+  };
 
-  getThread = async (threadID: string) => this.threads.get(threadID)
+  getThread = async (threadID: string) => this.threads.get(threadID);
 
   sendMessage = async (
     threadID: string,
     content: MessageContent,
-    options: MessageSendOptions,
+    options: MessageSendOptions
   ) => {
-    const { text } = content
+    const { text } = content;
 
     const message: Message = {
       id: options.pendingMessageID,
@@ -187,106 +194,184 @@ export default class ChatGPT implements PlatformAPI {
       senderID: SELF_ID,
       isSender: true,
       isDelivered: true,
-    }
+    };
+    const modelID = this.threads.get(threadID).extra?.aiModelId;
 
     this.eventHandler([
       {
         type: ServerEventType.USER_ACTIVITY,
         activityType: ActivityType.CUSTOM,
-        customLabel: 'thinking',
+        customLabel: "thinking",
         threadID,
-        participantID: 'ai',
+        participantID: modelID,
         durationMs: 30_000,
       },
-    ])
+    ]);
 
-    const msgs = this.messages.get(threadID) || []
-    msgs.push(message)
-    this.messages.set(threadID, msgs)
-    this.getAICompletion(threadID)
+    const msgs = this.messages.get(threadID) || [];
+    msgs.push(message);
+    this.messages.set(threadID, msgs);
+    this.getAIChatCompletion(threadID);
 
-    return [message]
-  }
+    const titleGenerated = this.threads.get(threadID).extra?.titleGenerated;
+    if (!titleGenerated) {
+      this.generateTitle(threadID, text);
+    }
 
-  getAICompletion = async (threadID: string) => {
+    return [message];
+  };
+
+  getAIChatCompletion = async (threadID: string) => {
     try {
-      const modelID = this.threads.get(threadID).extra?.aiModelId
+      const modelID = this.threads.get(threadID).extra?.aiModelId;
       const res = await this.openai.chat.completions.create({
         model: modelID,
         stream: true,
-        messages: (this.messages.get(threadID) || []).map(m => ({
-          role: m.senderID === SELF_ID ? 'user' : 'assistant',
+        messages: (this.messages.get(threadID) || []).map((m) => ({
+          role: m.senderID === SELF_ID ? "user" : "assistant",
           content: m.text,
         })),
-      })
+      });
 
       const aiMessage = {
         id: uuid(),
         timestamp: new Date(),
-        text: '',
-        senderID: 'ai',
+        text: "",
+        senderID: "ai",
         isSender: false,
-      }
+      };
 
       const stream = OpenAIStream(res, {
         onStart: () => {
-          this.messages.get(threadID).push(aiMessage)
+          this.messages.get(threadID).push(aiMessage);
           this.eventHandler([
             {
               type: ServerEventType.STATE_SYNC,
-              objectName: 'message',
-              mutationType: 'upsert',
+              objectName: "message",
+              mutationType: "upsert",
               objectIDs: { threadID },
               entries: [aiMessage],
             },
-          ])
+          ]);
         },
-        onToken: token => {
-          aiMessage.text += token
+        onToken: (token) => {
+          aiMessage.text += token;
           this.eventHandler([
             {
               type: ServerEventType.STATE_SYNC,
-              objectName: 'message',
-              mutationType: 'upsert',
+              objectName: "message",
+              mutationType: "upsert",
               objectIDs: { threadID },
               entries: [aiMessage],
             },
-          ])
+          ]);
         },
-      })
+        onCompletion: () => {
+          this.eventHandler([
+            {
+              type: ServerEventType.USER_ACTIVITY,
+              activityType: ActivityType.NONE,
+              threadID,
+              participantID: modelID,
+            },
+          ]);
+        },
+      });
 
-      const response = new StreamingTextResponse(stream)
-      const text = await response.text()
-      return text
+      const response = new StreamingTextResponse(stream);
+      const text = await response.text();
+      return text;
     } catch (e) {
       const errorMessage = {
-        id: 'error-' + uuid(),
+        id: "error-" + uuid(),
         timestamp: new Date(),
-        text: 'Error: ' + e.message,
-        senderID: 'none',
+        text: "Error: " + e.message,
+        senderID: "none",
         isAction: true,
-      }
+      };
       this.eventHandler([
         {
           type: ServerEventType.USER_ACTIVITY,
           activityType: ActivityType.NONE,
           threadID,
-          participantID: 'ai',
+          participantID: "ai",
         },
         {
           type: ServerEventType.STATE_SYNC,
-          objectName: 'message',
-          mutationType: 'upsert',
+          objectName: "message",
+          mutationType: "upsert",
           objectIDs: { threadID },
           entries: [errorMessage],
         },
-      ])
+      ]);
     }
-  }
+  };
 
-  sendActivityIndicator = (threadId: string) => {
-  }
+  generateTitle = async (threadID: string, firstUserPrompt: string) => {
+    try {
+      const res = await this.openai.completions.create({
+        model: "gpt-3.5-turbo-instruct",
+        prompt:
+          "Generate a 25 characters long title for this prompt:" +
+          firstUserPrompt,
+        stream: true,
+      });
 
-  sendReadReceipt = (threadId: string) => {
-  }
+      let generatedTitle = "";
+
+      // This is a Vercel/AI type error, it should be fine
+      // @ts-ignore
+      const stream = OpenAIStream(res, {
+        onStart: () => {
+          this.eventHandler([
+            {
+              type: ServerEventType.STATE_SYNC,
+              mutationType: "update",
+              objectName: "thread",
+              objectIDs: {},
+              entries: [
+                {
+                  id: threadID,
+                  title: generatedTitle,
+                },
+              ],
+            },
+          ]);
+        },
+        onToken: (token) => {
+          generatedTitle += token.includes(`"`)
+            ? token.replaceAll(`"`, "")
+            : token;
+
+          this.eventHandler([
+            {
+              type: ServerEventType.STATE_SYNC,
+              mutationType: "update",
+              objectName: "thread",
+              objectIDs: {},
+              entries: [
+                {
+                  id: threadID,
+                  title: generatedTitle.trim(),
+                },
+              ],
+            },
+          ]);
+        },
+        onCompletion: (completion) => {
+          this.threads.get(threadID).extra.titleGenerated = true;
+        },
+      });
+
+      const response = new StreamingTextResponse(stream);
+      const text = await response.text();
+      return text;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  sendActivityIndicator = (threadId: string) => {};
+
+  sendReadReceipt = (threadId: string) => {};
 }
--- src/auth.tsx
+++ src/auth.tsx
@@ -1,80 +1,82 @@
-import type { AuthProps } from '@textshq/platform-sdk'
-import React from 'react'
-import { PROVIDERS } from './constants'
+import type { AuthProps } from "@textshq/platform-sdk";
+import os from "os";
+import React from "react";
+import { PROVIDERS } from "./constants";
 
 const auth: React.FC<AuthProps> = ({ login }) => {
-  const [apiKey, setApiKey] = React.useState('')
-  const [label, setLabel] = React.useState('')
-  const [selectedProvider, setSelectedProvider] = React.useState('default')
+  const { username } = os.userInfo();
+  const [apiKey, setApiKey] = React.useState("");
+  const [label, setLabel] = React.useState("");
+  const [selectedProvider, setSelectedProvider] = React.useState("default");
 
   const handleLogin = () => {
-    if (apiKey !== '' && selectedProvider !== 'default') {
+    if (apiKey !== "" && selectedProvider !== "default") {
       login({
         custom: {
           apiKey,
           provider: selectedProvider,
           label,
+          username,
         },
-      })
+      });
     }
-  }
+  };
 
   return (
     <div>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          width: '100%',
-          justifyContent: 'center',
-          gap: '10px',
-          marginBottom: '20px',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+          justifyContent: "center",
+          gap: "10px",
+          marginBottom: "20px",
         }}
       >
-
         <div
           style={{
-            width: '70%',
-            marginLeft: 'auto',
-            marginRight: 'auto',
+            width: "70%",
+            marginLeft: "auto",
+            marginRight: "auto",
           }}
         >
           <label htmlFor="model">Provider</label>
           <select
             id="model"
             style={{
-              width: '100%',
-              borderRadius: '8px',
-              height: '30px',
-              background: 'transparent',
-              color: selectedProvider === 'default' ? '#757575' : 'white',
-              padding: '5px',
-              borderColor: '#343434',
-              outline: 'none',
+              width: "100%",
+              borderRadius: "8px",
+              height: "30px",
+              background: "transparent",
+              color: selectedProvider === "default" ? "#757575" : "white",
+              padding: "5px",
+              borderColor: "#343434",
+              outline: "none",
             }}
             value={selectedProvider}
-            onChange={event => setSelectedProvider(event.target.value)}
+            onChange={(event) => setSelectedProvider(event.target.value)}
           >
             <option
               value="default"
               disabled
               style={{
-                color: '#343434',
-                background: '#1c1c1c',
-                borderColor: '#343434',
+                color: "#343434",
+                background: "#1c1c1c",
+                borderColor: "#343434",
               }}
               hidden
             >
               Select a provider
             </option>
-            {PROVIDERS.map(provider => (
+            {PROVIDERS.map((provider) => (
               <option
                 value={provider.id}
                 style={{
-                  color: 'white',
-                  background: '#1c1c1c',
-                  borderColor: '#343434',
+                  color: "white",
+                  background: "#1c1c1c",
+                  borderColor: "#343434",
                 }}
               >
                 {provider.fullName}
@@ -84,50 +86,50 @@ const auth: React.FC<AuthProps> = ({ login }) => {
         </div>
         <div
           style={{
-            width: '70%',
+            width: "70%",
           }}
         >
-          <label htmlFor="api-key" style={{ width: '90%' }}>
+          <label htmlFor="api-key" style={{ width: "90%" }}>
             API Key
           </label>
           <input
             id="api-key"
             type="text"
             value={apiKey}
-            onChange={event => setApiKey(event.target.value)}
-            style={{ width: '100%' }}
+            onChange={(event) => setApiKey(event.target.value)}
+            style={{ width: "100%" }}
             placeholder="Your OpenAI API Key"
           />
         </div>
         <div
           style={{
-            width: '70%',
+            width: "70%",
           }}
         >
-          <label htmlFor="label" style={{ width: '90%' }}>
+          <label htmlFor="label" style={{ width: "90%" }}>
             Label (optional)
           </label>
           <input
             id="label"
             type="text"
             value={label}
-            onChange={event => setLabel(event.target.value)}
-            style={{ width: '100%' }}
+            onChange={(event) => setLabel(event.target.value)}
+            style={{ width: "100%" }}
             placeholder="Work, Personal, etc."
           />
         </div>
         <div
           style={{
-            width: '70%',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            marginTop: '10px',
+            width: "70%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: "10px",
           }}
         >
           <button
             type="button"
             style={{
-              width: '100%',
+              width: "100%",
             }}
             onClick={handleLogin}
           >
@@ -136,10 +138,10 @@ const auth: React.FC<AuthProps> = ({ login }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default auth
+export default auth;
 
 /*
 type Status = 'none' | 'qrcode' | 'authenticating' | 'success'
